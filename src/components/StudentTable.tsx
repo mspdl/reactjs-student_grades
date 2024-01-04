2840f8e367ab1e99ed75a14608373fa83f69153b
Morgan <morgan.espindola@gmail.com>
2024-01-04 06:42:33 -0300

    Stylized table

--- src/components/StudentTable.tsx
+++ src/components/StudentTable.tsx
@@ -9,8 +9,8 @@ const StudentTable = ({ students }: StudentTableProps) => {
     <div>
       <table className="w-full border border-gray-600 rounded-md overflow-hidden">
         <thead>
-          <tr className="text-left border-b border-gray-600 bg-gray-800">
-            <th className="p-3">Name</th>
+          <tr className="border-b border-gray-600 bg-gray-800">
+            <th className="text-left p-3">Name</th>
             <th className="p-3">Status</th>
             <th className="p-3">Grade 1</th>
             <th className="p-3">Grade 2</th>
@@ -19,21 +19,36 @@ const StudentTable = ({ students }: StudentTableProps) => {
         </thead>
         <tbody>
           {students.map((student) => (
-            <tr key={student.id}>
-              <td>
-                <img src={student.avatar} alt={student.name} />
+            <tr
+              key={student.id}
+              className="text-center text-gray-800 bg-gray-400 border-b border-gray-600"
+            >
+              <td className="p-3 flex item-center">
+                <img
+                  src={student.avatar}
+                  alt={student.name}
+                  className="w-10 h-10 rounded-full mr-3"
+                />
                 <div>
-                  <div>{student.name}</div>
+                  <div className="font-bold text-left">{student.name}</div>
                   <div>{student.email}</div>
                 </div>
               </td>
-              <td>
-                {student.active && <div>Active</div>}
-                {!student.active && <div>Inactive</div>}
+              <td className="p-3">
+                {student.active && (
+                  <div className="px-2 py-1 inline-block rounded-md border border-green-800 bg-green-600 text-white text-xs">
+                    Active
+                  </div>
+                )}
+                {!student.active && (
+                  <div className="px-2 py-1 inline-block rounded-md border border-red-800 bg-red-600 text-white text-xs">
+                    Inactive
+                  </div>
+                )}
               </td>
-              <td>{student.grade1}</td>
-              <td>{student.grade2}</td>
-              <td>
+              <td className="p-3">{student.grade1.toFixed(1)}</td>
+              <td className="p-3">{student.grade2.toFixed(1)}</td>
+              <td className="p-3 font-bold">
                 {student.active
                   ? ((student.grade1 + student.grade2) / 2).toFixed(1)
                   : "--"}
